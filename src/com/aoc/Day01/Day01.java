a3bbc0e3846abd50842fadda609824e2b1142f09
LeszczyTom <leszczytom@gmail.com>
2021-12-15 00:07:57 +0100

    part1

--- src/com/aoc/Day01/Day01.java
+++ src/com/aoc/Day01/Day01.java
@@ -2,13 +2,28 @@ package com.aoc.Day01;
 
 import com.aoc.Day;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day01 implements Day {
 
     @Override
     public String part1(List<String> input) {
-        return input.isEmpty() ? "" : input.get(0);
+        List<Integer> integerList = new ArrayList<>();
+        for(String j : input) {
+            integerList.add(Integer.parseInt(j));
+        }
+
+        int cpt = 0, before = 0;
+        for(int j : integerList) {
+            if(j > before && before != 0) {
+                cpt++;
+            }
+            before = j;
+        }
+        return input.isEmpty() ? "" : cpt + "";
     }
 
     @Override
