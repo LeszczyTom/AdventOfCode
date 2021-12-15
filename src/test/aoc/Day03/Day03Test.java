792f4f037e14b7dbe3449a499f8bd8925181418f
LeszczyTom <leszczytom@gmail.com>
2021-12-15 14:54:12 +0100

    Ajout test jour 3 - part 2

--- src/test/aoc/Day03/Day03Test.java
+++ src/test/aoc/Day03/Day03Test.java
@@ -37,13 +37,25 @@ public class Day03Test {
 
     @Test
     public void testPart2(){
-        // Given
-        List<String> input = Collections.singletonList("test");
+        List<String> input = Arrays.asList(
+                "00100",
+                "11110",
+                "10110",
+                "10111",
+                "10101",
+                "01111",
+                "00111",
+                "11100",
+                "10000",
+                "11001",
+                "00010",
+                "01010"
+        );
 
         // When
         String result = new Day03().part2(input);
 
         // Then
-        assertEquals(input.get(0), result);
+        assertEquals("230", result);
     }
 }
